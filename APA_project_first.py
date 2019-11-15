ae045bfbb631b9c1f9f2a746cf3c28b33a78394e
JTermens <55915367+JTermens@users.noreply.github.com>
2019-11-15 17:26:04 +0100

    APA_project_first.py and 4_coexpr_geo_v2.zip
    APA_project_first.py is the first version of the community detection and sorting project.
    4_coexpr_geo_v2.zip is the network used to test it. Corresponds to the 4th network of the DREAM challenge

---
+++ APA_project_first.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+import networkx as nx
+from networkx.algorithms import community
+import community
+
+
+
+def filtered(filename,threshold = 0.55):
+	df = pd.read_csv(filename, sep="\t")
+	df_filtered=df.loc[ (df['edge'] > threshold) & (df['gene1'] != df['gene2']) ]
+	#print(df_filtered)
+	return df_filtered
+
+def network(df):
+	G=nx.from_pandas_edgelist(df_filtered, 'gene1', 'gene2')
+	#print(len(G.nodes()))
+	return G
+#-----------------------------------------
+def louvain(G):
+	# Starting with an initial partition of the graph and running the Louvain algorithm for Community Detection
+	partition=community.best_partition(G, weight='MsgCount')
+	#print('Completed Louvain algorithm .. . . ' )
+	values=[partition.get(node) for node in G.nodes()]
+	list_com=partition.values()
+
+	# Creating a dictionary like {community_number:list_of_participants}
+	dict_nodes={}
+
+	# Populating the dictionary with items
+	for each_item in partition.items():
+	    community_num=each_item[1]
+	    community_node=each_item[0]
+	    if community_num in dict_nodes:
+	        value=str(dict_nodes.get(community_num)) + ' | ' + str(community_node)
+	        dict_nodes.update({community_num:value})
+	    else:
+	        dict_nodes.update({community_num:community_node})
+
+	# Creating a new graph to represent the communities created by the Louvain algorithm
+	# matplotlib.rcParams['figure.figsize']= [12, 8]
+	G_comm=nx.Graph()
+	# print('im here')
+
+
+	# Populating the data from the node dictionary created earlier
+	G_comm.add_nodes_from(dict_nodes)
+	#print(G_comm.nodes())
+	# Calculating modularity and the total number of communities
+	mod=community.modularity(partition,G)
+	print("Total number of Communities=", len(G_comm.nodes()))
+	print("Modularity: ", mod)
+
+	#print(dict_nodes.keys())
+	return dict_nodes
+	# Creating the Graph and also calculating Modularity
+	#pos_louvain=nx.spring_layout(G_comm)
+
+
+class Community(object):
+    def __init__(self,dens,size,rel_dens,max_btw,avg_btw,max_centr,avg_centr,max_load,avg_load,mod):
+        self.dens = dens
+        self.size = size
+        self.rel_dens = rel_dens
+        self.max_btw = max_btw
+        self.avg_btw = avg_btw
+        self.max_centr = max_centr
+        self.avg_centr = avg_centr
+        self.max_load = max_load
+        self.avg_load = avg_load
+        self.mod = mod
+
+
+def ComputeFeatures(set_nodes,df_network,key1='node1',key2='node2',weight = None):
+
+    df_comm = df_network[(df_network[key1].isin(set_nodes)) & (df_network[key2].isin(set_nodes))] # community data frame
+    g_comm = nx.from_pandas_edgelist(df_comm,key1,key2,edge_attr = weight) # community graph
+    g_network = nx.from_pandas_edgelist(df_network,key1,key2,edge_attr = weight) # network graph
+
+    dict_comm = {node:0 if node in set_nodes else 1 for node in g_network.nodes()} # necessary to calculate the modularity
+
+    # Features:
+
+    dens = nx.density(g_comm) # 1. density
+    size = len(set_nodes) # 2. size
+    rel_dens = dens/size # 3. relative density
+
+    btw_centr = nx.betweenness_centrality(g_comm, normalized=True, weight=weight) # dict with nodes' betweenness centrality
+    max_btw = max(btw_centr.values()) # 4. max betweenness centrality
+    avg_btw = sum(btw_centr.values())/size # 5. average betweenness centrality
+
+    degree_centr = nx.degree_centrality(g_comm) # dict with nodes' degree centrality
+    max_centr = max(degree_centr.values()) # 6. max degree centrality
+    avg_centr = sum(degree_centr.values())/size # 7. average degree centrality
+
+    load_centr = nx.load_centrality(g_comm, normalized=True, weight=weight) # dict with nodes' load centrality
+    max_load = max(load_centr.values()) # 8. max load centrality
+    avg_load = sum(load_centr.values())/size # 9. average load centrality
+
+    mod = community.modularity(dict_comm,g_network) # 10. community modularity within the network
+
+    comm = Community(dens,size,rel_dens,max_btw,avg_btw,max_centr,avg_centr,max_load,avg_load,mod)
+
+    return comm
+
+
+if __name__=='__main__':
+
+	#df = pd.read_csv("4_coexpr_geo_v2.txt", sep="\t")
+	df_filtered = filtered("4_coexpr_geo_v2.txt")
+	print('Building network...')
+	network = network(df_filtered)
+	print('Community detection in course...')
+	dict_nodes = louvain(network)
+	print('Extracting features...')
+	for comm_num in dict_nodes.keys():
+	#for comm_num in range(1,len(G_comm.nodes())):
+	    set_nodes = dict_nodes[comm_num].split(' | ')
+	    ComputeFeatures(set_nodes,df_filtered,'gene1','gene2','edge')
+	print('Feature extraction completed...')
